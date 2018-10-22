06bac2c571a888cec6ceb2fbbfad91b5302243c5
fedes112 <fedes112@users.noreply.github.com>
2018-10-21 22:03:33 -0300

    chequeador

--- src/main/scala/ar.edu.unq.o3.Pifyo/Pifyo.scala
+++ src/main/scala/ar.edu.unq.o3.Pifyo/Pifyo.scala
@@ -2,6 +2,8 @@ package ar.edu.unq.o3.Pifyo
 
 import ar.edu.unq.o3.Pifyo.Pifyo._
 
+import scala.collection.mutable.ArrayBuffer
+
 object Pifyo {
 
 
@@ -19,40 +21,108 @@ object Pifyo {
   case class MayorOIgual(numeroA: Numero, numeroB: Numero) extends Instruccion
 
 
-  def Chequeador : ( Programa,  Chequeo) => Problema = (prg, cheq) => cheq.chequear(prg)
+  def Chequeador : ( Programa,  Chequeo) => Problema = (prg, cheq) => cheq.chequear(prg).getOrElse(Problema("","",""))
 
-
+  def chequeadorMultiple : (Programa, List[Chequeo]) => ArrayBuffer[Problema] = {
+    (prog, chequeos) =>
+      var problemasEncontrados = ArrayBuffer[Problema]()
+      chequeos.foldLeft(problemasEncontrados){
+        (acc,f) => acc += Chequeador(prog,f)
+      }
+       problemasEncontrados
+  }
   case class Problema(descripcion:String, gravedad: String, instruccion:String)
   case class Programa(instruccion: Instruccion)
 
   trait Instruccion
 
   trait Chequeo {
-    def chequear : Programa => Problema
+    def chequear : Programa =>Option[Problema]
 
   }
 
 
   case class SumaCero() extends  Chequeo {
 
-   override def chequear : Programa=> Problema = prg => prg match  {
-      case Pifyo.Programa(Suma(Numero(0),_)) => Problema("Operacion redundante de suma 0","Advertencia","Suma")
-      case Pifyo.Programa(Suma(_,Numero(0))) => Problema("Operacion redundante de suma 0","Advertencia","Suma")
-      case  Pifyo.Programa(_) => Problema("","","")
+   override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Suma(Numero(0),_)) => Some(Problema("Operacion redundante de suma 0","Advertencia","Suma"))
+      case Pifyo.Programa(Suma(_,Numero(0))) => Some(Problema("Operacion redundante de suma 0","Advertencia","Suma"))
+      case  Pifyo.Programa(_) => None
    }
 
   }
 
-  type OperacionEnteros = (Int, Int) => Int
-  def _sumar4: OperacionEnteros = (a, b) => a + b
+  case class RestarCero() extends  Chequeo {
 
+    override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Resta(_,Numero(0))) => Some(Problema("Operacion redundante de resta 0","Advertencia","Resta"))
+      case  Pifyo.Programa(_) => None
+    }
+
+  }
+
+  case class DividirUno() extends  Chequeo {
+
+    override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Division(_,Numero(1))) => Some(Problema("Operacion redundante de division 1","Advertencia","Division"))
+      case  Pifyo.Programa(_) => None
+    }
+
+  }
+
+  case class MultiplicarUno() extends  Chequeo {
+
+    override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Multiplicacion(_,Numero(1))) => Some(Problema("Operacion redundante de Multiplicacion 1","Advertencia","Multiplicacion"))
+      case  Pifyo.Programa(_) => None
+    }
+
+  }
+
+  case class DivisionPorCero() extends  Chequeo {
+
+    override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Division(_,Numero(0))) => Some(Problema("Division Por Cero","Error","Division"))
+      case  Pifyo.Programa(_) => None
+    }
+
+  }
+
+  case class ComparacionDosNumerosIguales() extends  Chequeo {
+
+    override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Igual(Numero(_),Numero(_))) => Some(Problema("Comparacion de dos numeros iguales","Error","Igual"))
+      case  Pifyo.Programa(_) => None //preguntar iguales
+    }
+
+  }
+
+  case class ComparacionSiempreDaTrue() extends  Chequeo {
+
+    override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Mayor(Numero(a:Int),Numero(b:Int)))  => Some(Problema("Comparacion que siempre da true","Error","Division"))
+      case  Pifyo.Programa(_) => None
+    }
+
+  }
+
+  case class ComparacionSiempreDaFalse() extends  Chequeo {
+
+    override def chequear : Programa=> Option[Problema] = prg => prg match  {
+      case Pifyo.Programa(Menor(_,Numero(0))) => Some(Problema("Comparacion que siempre da false","Error","Division"))
+      case  Pifyo.Programa(_) => None
+    }
+
+  }
 }
 
 
 object pruebaSumaCero extends App {
 
-  val programa = Programa(Suma((Numero(0)),(Numero(3))))
+  val programa = Programa(Suma((Numero(2)),(Numero(2))))
   val chequeo = SumaCero()
-  val chequeador = Chequeador(programa, chequeo )
+  val chequeo2 = ComparacionDosNumerosIguales()
+  var chequeos = List[Chequeo](chequeo,chequeo2)
+  val chequeador = chequeadorMultiple(programa, chequeos)
   println(chequeador)
 }
